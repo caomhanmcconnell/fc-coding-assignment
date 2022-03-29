40b734d280e9a5daff67e7089ae88fa288522f7d
caomhanmcconnell <caomhanmcconnell@gmail.com>
2022-03-30 00:36:53 +0100

    commit changes from day (progress on both ui & api)

---
+++ API/product-list/productList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+public class productTable
+{
+	private void CreateProductTable()
+	{
+		DataTable productTable = new DataTable("Products");
+		DataColumn dtColumn;
+		DataRow newDataRow;
+
+		//Create Product ID column
+		dtColumn = new DataColumn();
+		dtColumn.DataType = typeof(Int32);
+		dtColumn.ColumnName = "id";
+		dtColumn.Caption = "Product ID";
+		dtColumn.ReadOnly = false;
+		dtColumn.Unique = true;
+		//Add column to product table
+		productTable.Columns.Add(dtColumn);
+
+		//Create Product column
+		dtColumn = new DataColumn();
+		dtColumn.DataType = typeof(String);
+		dtColumn.ColumnName = "product";
+		dtColumn.Caption = "Product";
+		dtColumn.ReadOnly = false;
+		dtColumn.Unique = false;
+		//Add column to product table
+		productTable.Columns.Add(dtColumn);
+
+		//Create January 2018 column
+		dtColumn = new DataColumn();
+		dtColumn.DataType = typeof(double);
+		dtColumn.ColumnName = "jan 2018";
+		dtColumn.Caption = "January 2018";
+		dtColumn.ReadOnly = false;
+		dtColumn.Unique = true;
+		//Add column to product table
+		productTable.Columns.Add(dtColumn);
+
+		//Create Feburary 2018 column
+		dtColumn = new DataColumn();
+		dtColumn.DataType = typeof(double);
+		dtColumn.ColumnName = "feb 2018";
+		dtColumn.Caption = "Feburary 2018";
+		dtColumn.ReadOnly = false;
+		dtColumn.Unique = true;
+		//Add column to product table
+		productTable.Columns.Add(dtColumn);
+
+		//Create March 2018 column
+		dtColumn = new DataColumn();
+		dtColumn.DataType = typeof(double);
+		dtColumn.ColumnName = "mar 2018";
+		dtColumn.Caption = "March 2018";
+		dtColumn.ReadOnly = false;
+		dtColumn.Unique = true;
+		//Add column to product table
+		productTable.Columns.Add(dtColumn);
+
+		//ensuring product id is primary key
+		DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+		PrimaryKeyColumns[0] = productTable.Columns["id"];
+		productTable.PrimaryKey = PrimaryKeyColumns;
+
+
+		//Creating new DataSet
+		dtSet = new DataSet();
+
+		//Add product table to DataSet
+		dtSet.Tables.Add(productTable);
+
+		//Populating product table with data
+		//
+		//need to read in file of products then append to table
+		//with each product using data from file...
+
+
+	}
+}
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -4,59 +4,39 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Product Price List</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h2 className="title">
+          FC Coding Task
+        </h2>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+              <div>
+                  <h3>
+                      Customer Price List
+                  </h3>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                  <table className="grid">
+                      <thead>
+                          <th>Product ID</th>
+                          <th>Product</th>
+                          <th>January 2018</th>
+                          <th>Feburary 2018</th>
+                          <th>March 2018</th>
+                          <th>Actions</th>
+                      </thead>
+                      <tbody>
+                      </tbody>
+                  </table>
+              </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        
       </footer>
 
       <style jsx>{`
@@ -190,20 +170,6 @@ export default function Home() {
         }
       `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
